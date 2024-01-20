fdd5ac26d4db024ff14a77cf59b4f866c9a9b634
tombe <mail@thomas-ruff.de>
2024-01-20 04:24:54 +0100

    Work in progress

--- app/main.py
+++ app/main.py
@@ -27,8 +27,10 @@ class Values:
     def __init__(self):
         self.token: str = ''
         self.session = requests.Session()
-        self.backend_url = "http://localhost:8000/"
-        self.ws_url = "ws://localhost:8000/ws/"
+        # self.backend_url = "http://localhost:8000/"
+        # self.ws_url = "ws://localhost:8000/ws/"
+        self.backend_url = "https://clinic-clown-control.onrender.com/"
+        self.ws_url = "wss://clinic-clown-control.onrender.com/ws/"
         self.team_of_actors: schemas.TeamOfActorsShow | None = None
 
     def set_session_token(self, token: str):
