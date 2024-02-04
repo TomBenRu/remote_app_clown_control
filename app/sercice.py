7dd1825061643ffffc2c5ada1c297f8a0c2eae55
tombe <mail@thomas-ruff.de>
2024-02-04 15:23:55 +0100

    Work in progress

--- app/sercice.py
+++ app/sercice.py
@@ -1,18 +1,18 @@
 import time
 
-from jnius import autoclass
-from oscpy.client import OSCClient
+# from jnius import autoclass
+# from oscpy.client import OSCClient
 
 
-PythonService = autoclass('org.kivy.android.PythonService')
-PythonService.mService.setAutoRestartService(True)
+# PythonService = autoclass('org.kivy.android.PythonService')
+# PythonService.mService.setAutoRestartService(True)
 
-CLIENT = OSCClient('localhost', 3002)
+# CLIENT = OSCClient('localhost', 3002)
 
 t = 0
 while True:
     time.sleep(1)
     t += 1
     print(f'{t=}')
-    CLIENT.send_message('/message', [f'message {t}'])
+#     CLIENT.send_message('/message', [f'message {t}'])
 
