9f2636e7b19bbca3923547181f261cb1114d07f7
tombe <mail@thomas-ruff.de>
2024-02-08 12:47:08 +0100

    Work in progress

--- app/service.py
+++ app/service.py
@@ -1,9 +1,14 @@
 import json
+import pickle
+import ssl
+import threading
 import time
 
+import websocket
 # from jnius import autoclass
 from oscpy.client import OSCClient
 from oscpy.server import OSCThreadServer
+from websocket import WebSocketApp
 
 # PythonService = autoclass('org.kivy.android.PythonService')
 # PythonService.mService.setAutoRestartService(True)
@@ -15,6 +20,56 @@ SERVER = OSCThreadServer()
 def handle_call(call):
     call = json.loads(call.decode('utf-8'))
     print(f'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> call: {call}')
+
+
+class OscHandler:
+    def __init__(self):
+        self.client = CLIENT
+        self.server = SERVER
+        self.server.listen(address=b'localhost', port=3000, default=True)
+
+        self.server.bind(b'/call', self.handle_call)
+        self.server.bind(b'/connect', self.handle_connect)
+
+        self.ws: WebSocketApp | None = None
+
+    def handle_call(self, call):
+        ...
+
+    def handle_ws_message(self, ws, message):
+        self.client.send_message(b'/ws_message', [message.encode('utf-8'),])
+
+    def handle_ws_error(self, ws, error):
+        self.client.send_message(b'/ws_error', [error.encode('utf-8'),])
+
+    def handle_ws_open(self, ws):
+        ws_pickled = pickle.dumps(ws)
+        self.client.send_message(b'/ws_opened', [ws_pickled])
+
+    def handle_ws_close(self, ws, close_status_code, close_msg):
+        self.client.send_message(b'/ws_closed', [close_status_code.encode('utf-8'), close_msg.encode('utf-8'),])
+
+    def handle_already_open_ws_connection(self):
+        self.client.send_message(b'/ws_already_open', [1,])
+
+    def handle_connect(self, values):
+        self.open_ws_connection(values[0].decode('utf-8'),
+                                values[1].decode('utf-8'),
+                                values[2].decode('utf-8'))
+
+    def open_ws_connection(self, ws_url: str, token: str, team_of_actors_id: str):
+        if self.ws:
+            self.handle_already_open_ws_connection()
+            return
+        self.ws = websocket.WebSocketApp(ws_url,
+                                         on_message=self.handle_ws_message,
+                                         on_error=self.handle_ws_error,
+                                         on_close=self.handle_ws_close,
+                                         cookie=f'clown-call-auth={token}',
+                                         header={'team_of_actors_id': team_of_actors_id})
+        self.ws.on_open = self.handle_ws_open
+        threading.Thread(target=self.ws.run_forever,
+                         kwargs={"sslopt": {"cert_reqs": ssl.CERT_NONE}, 'reconnect': 5}).start()
 
 
 if __name__ == '__main__':
