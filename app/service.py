54d36d18aa70f174a52683a8a086e9f3f96af44d
tombe <mail@thomas-ruff.de>
2024-02-08 23:13:03 +0100

    Work in progress

--- app/service.py
+++ app/service.py
@@ -41,6 +41,7 @@ class OscHandler:
     def handle_ws_message(self, ws, message):
         print(f'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ws message {message=}')
         if json.loads(message).get('message'):
+            print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> VIBRATE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
             self.vibrate()
         self.client.send_message(b'/ws_message', [message.encode('utf-8'),])
 
