2886c7f80d9fb61f7e79b6f8012d5f46226d5f49
tombe <mail@thomas-ruff.de>
2024-02-10 21:02:29 +0100

    Work in progress

--- app/main.py
+++ app/main.py
@@ -349,10 +349,7 @@ class ClownControlApp(MDApp):
         return sm
 
     def service_is_running(self):
-        from android.config import SERVICE_CLASS_NAME
-
-        PythonService = autoclass(SERVICE_CLASS_NAME)
-        mActivity = PythonService.mService
+        from android import mActivity
         context = mActivity.getApplicationContext()
         manager = cast('android.app.ActivityManager',
                        mActivity.getSystemService(context.ACTIVITY_SERVICE))
