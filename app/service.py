d666dd527b70d9e3bacb8a38d99d5371b45a254b
tombe <mail@thomas-ruff.de>
2024-02-11 16:51:14 +0100

    Work in progress

--- app/service.py
+++ app/service.py
@@ -5,12 +5,35 @@ import time
 
 import plyer
 import websocket
+from jnius import autoclass
 from oscpy.client import OSCClient
 from oscpy.server import OSCThreadServer
 from websocket import WebSocketApp
 
 CLIENT = OSCClient(b'localhost', 3002)
 SERVER = OSCThreadServer()
+
+
+class NotificationAndroid:
+    def __init__(self, title: str, message: str):
+        self.title = title
+        self.message = message
+
+    def notify(self, *args):
+        AndroidString = autoclass('java.lang.String')
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+        NotificationBuilder = autoclass('android.app.Notification$Builder')
+        Drawable = autoclass(f"{PythonActivity.mActivity.getPackageName()}.R$drawable")
+        # Drawable = autoclass('org.test.notify.R$drawable')  # jnius.autoclass("{}.R$drawable".format(service.getPackageName()))
+        icon = Drawable.icon
+        notification_builder = NotificationBuilder(PythonActivity.mActivity)
+        notification_builder.setContentTitle(AndroidString(self.title.encode('utf-8')))
+        notification_builder.setContentText(AndroidString(self.message.encode('utf-8')))
+        notification_builder.setSmallIcon(icon)
+        notification_builder.setAutoCancel(True)
+        Context = autoclass('android.content.Context')
+        notification_service = PythonActivity.mActivity.getSystemService(Context.NOTIFICATION_SERVICE)
+        notification_service.notify(0, notification_builder.build())
 
 
 class OscHandler:
@@ -35,6 +58,7 @@ class OscHandler:
 
     def handle_ws_message(self, ws, message):
         print(f'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ws message {message=}')
+        NotificationAndroid('Clown-Call', message).notify()
         if message_str := json.loads(message).get('message'):
             print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> VIBRATE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
             try:
