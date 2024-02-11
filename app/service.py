45fe7b79b41195fa732402e00a5b6d305c6bb1df
tombe <mail@thomas-ruff.de>
2024-02-11 18:02:33 +0100

    Work in progress

--- app/service.py
+++ app/service.py
@@ -20,19 +20,20 @@ class NotificationAndroid:
         self.message = message
 
     def notify(self, *args):
+        from android import mActivity
         AndroidString = autoclass('java.lang.String')
-        PythonActivity = autoclass('org.renpy.android.PythonActivity')
+        # PythonActivity = autoclass('org.kivy.android.PythonActivity')
         NotificationBuilder = autoclass('android.app.Notification$Builder')
-        Drawable = autoclass(f"{PythonActivity.mActivity.getPackageName()}.R$drawable")
+        Drawable = autoclass(f"{mActivity.getPackageName()}.R$drawable")
         # Drawable = autoclass('org.test.notify.R$drawable')  # jnius.autoclass("{}.R$drawable".format(service.getPackageName()))
         icon = Drawable.icon
-        notification_builder = NotificationBuilder(PythonActivity.mActivity)
+        notification_builder = NotificationBuilder(mActivity)
         notification_builder.setContentTitle(AndroidString(self.title.encode('utf-8')))
         notification_builder.setContentText(AndroidString(self.message.encode('utf-8')))
         notification_builder.setSmallIcon(icon)
         notification_builder.setAutoCancel(True)
         Context = autoclass('android.content.Context')
-        notification_service = PythonActivity.mActivity.getSystemService(Context.NOTIFICATION_SERVICE)
+        notification_service = mActivity.getSystemService(Context.NOTIFICATION_SERVICE)
         notification_service.notify(0, notification_builder.build())
 
 
