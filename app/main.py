252fb827705c42a3033f11e6049ba54e3808fd6d
tombe <mail@thomas-ruff.de>
2024-02-15 14:28:48 +0100

    Work in progress

--- app/main.py
+++ app/main.py
@@ -282,7 +282,7 @@ class ChatScreen(Screen):
                                                                   f" hat den Chat betreten.\n")
                 new_chat_tab = ChatTab(tab_label_text=f'{values.departments_of_location[department_id]["name"]}',
                                        department_id=department_id, osc_client=self.client, notification_client=self.notification_client, tab_pos=len(self.chat_tabs))
-                new_chat_tab.ids.output.text += self.text_closed_departments[department_id]
+                new_chat_tab.ids.output.text = self.text_closed_departments[department_id]
                 self.chat_tabs[department_id] = new_chat_tab
                 self.ids.chat_tabs.add_widget(new_chat_tab)
             else:
