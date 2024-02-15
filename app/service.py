e8cfcef709fe7f430796b324518ea6f26a883909
tombe <mail@thomas-ruff.de>
2024-02-15 16:02:54 +0100

    Work in progress

--- app/service.py
+++ app/service.py
@@ -55,8 +55,8 @@ class OscHandler:
         self.client.send_message(b'/ws_message', [message.encode('utf-8'),])
         if (self.greeting_message and json.loads(message).get('joined')
                 and (department_id := json.loads(message).get('department_id'))):
-            self.handle_call(self.greeting_message, department_id)
-            self.client.send_message(b'/put_message_to_output', [department_id.encode('utf-8'), self.greeting_message])
+            self.handle_call(self.greeting_message, department_id.encode('utf-8'))
+            # self.client.send_message(b'/put_message_to_output', [department_id.encode('utf-8'), self.greeting_message])
 
     def handle_ws_error(self, ws, error):
         print(f'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ws error {error=}')
