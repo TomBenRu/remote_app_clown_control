a6493193e5d0de5504baed6fd2cae91919dc9040
tombe <mail@thomas-ruff.de>
2024-03-02 17:13:04 +0100

    New chat layout

--- app/main.py
+++ app/main.py
@@ -263,7 +263,7 @@ class CustomLabel(Label):
         self.text_size = (self.width, None)
 
 
-class MessageBubble(BoxLayout):
+class MessageBubble(AnchorLayout):
     text = StringProperty('')  # Definieren Sie die text-Eigenschaft
     mode = StringProperty('incoming')  # Definieren Sie die Mode-Eigenschaft (incoming, outgoing, info
 
